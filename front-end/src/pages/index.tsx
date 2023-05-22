b2926bd5c442856fb4cf1f20bd701e16e91419b3
Roger Franco <rogerfrancogalt@gmail.com>
2023-05-22 11:41:09 -0400

    Rotas privadas, impedindo o acesso a algumas páginas por alguém já logado

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -8,6 +8,7 @@ import Link from 'next/link'
 import { AuthContext } from '@/contexts/AuthContext'
 import { useContext, FormEvent, useState } from 'react'
 import { toast } from 'react-toastify'
+import {canSSRGuest} from '../utils/canSSRGuest'
 
 export default function Home() {
   const {signIn} = useContext(AuthContext)
@@ -72,3 +73,9 @@ export default function Home() {
     
   )
 }
+
+export const getServerSideProps = canSSRGuest(async (ctx) => {
+  return {
+    props: {}
+  }
+})
